56261cc96827eaf6120d6e713c55f598a870386d
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2017-02-11 11:45:58 +0100

    add binary files to edit and fix cleaning inputs

--- updator.py
+++ updator.py
@@ -13,5 +13,9 @@ if __name__ == "__main__":
     with open(argv[1], "r") as binary_table_file:
         for line in binary_table_file.readlines():
             binary_file, dynamic_lib = line.split("=")
+            binary_file = binary_file.strip()
+            dynamic_lib = dynamic_lib.strip()
             old_path, new_path = dynamic_lib.split("->")
+            old_path = old_path.strip()
+            new_path = new_path.strip()
             system("install_name_tool -change " + old_path + " " + new_path + " " + binary_file)
